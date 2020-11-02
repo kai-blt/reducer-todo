e4f2a659cb051122890e3092b2b1039b41175088
kai-blt <blaitong@gmail.com>
2020-11-02 14:18:52 -0800

    added form stying

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,6 +2,41 @@ import React, { useReducer } from 'react';
 import reducer from './reducers'
 import { handleInput, handleClick, setCompleted, clearCompleted } from './actions'
 import ToDoItem from './ToDoItem'
+import styled from 'styled-components'
+
+
+const StyledForm = styled.form`
+    background-color: #222;
+    padding: 2%;
+    color: #fff;
+
+    input {
+        border-radius: 5px;
+        border: 1px solid #000;
+        padding: 1%;
+        margin: 0 2%;
+    }
+
+    button {
+        color: #fff;
+        font-size: 1rem;
+        font-weight: 700;
+        border-radius: 5px;
+        border: 1px solid #000;
+        padding: 1% 2%;
+        margin: 0 0.5%;
+    }
+
+    .add-btn {
+        background-color: green;
+    }
+
+    .clear-btn {
+        background-color: red;
+    }
+
+`;
+
 
 export default function ToDoList(props) {
     //initial state
@@ -15,18 +50,18 @@ export default function ToDoList(props) {
 
     return (
         <div>
-            <form>
-                <label>To Do:
+            <StyledForm>
+                <label><h1>TO DO</h1><br/>
                     <input 
                         type="text"
                         name="todo"
                         value={state.current_input}
                         onChange={(e) => dispatch(handleInput(e))}
                     />
-                    <button onClick={(e) => dispatch(handleClick(e, state.current_input))}>Add</button>
-                    <button onClick={(e) => dispatch(clearCompleted(e, state.list))}>Clear Completed</button>
+                    <button className="add-btn" onClick={(e) => dispatch(handleClick(e, state.current_input))}>Add</button>
+                    <button className="clear-btn" onClick={(e) => dispatch(clearCompleted(e, state.list))}>Clear Completed</button>
                 </label>                
-            </form>
+            </StyledForm>
             {state.list.length > 0 ?
                 state.list.map(item => <ToDoItem key={item.id} item={item.item} id={item.id} completed={item.completed} setCompleted={(e) => dispatch(setCompleted(e, item.id, state.list))}/>)
             :
