070654065440ecc34f945952065b0662d0c2db32
kai-blt <blaitong@gmail.com>
2020-11-02 14:12:08 -0800

    added strikethrough ability and styling

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Strike = styled.del`
+    color: red;
+`;
+
 
 export default function ToDoItem(props) {
     const { id, item, completed, setCompleted } = props;
     return (
         <div onClick={setCompleted}>
-            <h2>{item}</h2>
+            {completed ? <Strike><h2>{item}</h2></Strike> : <h2>{item}</h2> }
         </div>
     )
 }
